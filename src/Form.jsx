13ff8beb0a1f5a5cb0d917779cdc03e3f6cf1e9d
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-23 11:05:20 +0800

    Add jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,5 +1,18 @@
 export default function Form(){
     return(
-        <input type="text" />
+        <form>
+            <label>
+                <span>Name</span>
+                <input type="text"></input>
+            </label>
+            <label>
+                <span>Email</span>
+                <input type="text"></input>
+            </label>
+            <label>
+                <span>Address</span>
+                <input type="text"></input>
+            </label>
+        </form>
     )
 }
