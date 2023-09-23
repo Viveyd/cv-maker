f5bda33af1d5b453e22fbaf20501dd4e8e7f8325
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-23 10:35:58 +0800

    Init Form and Preview modules

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Preview from './Preview'
 
 function App() {
 
   return (
     <>
+      <Form/>
+      <Preview/>
     </>
   )
 }
