4c2e35f12e29da096f88f8e0899f9c1b7f29a496
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-30 14:27:34 +0800

    Separate components

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,10 +1,57 @@
 export default function Skills({ data, updater, active, toggler }) {
-  function updateData(e, key) {
-    updater({ ...data, [key]: e.target.value });
+  function addData(e) {
+    e.preventDefault();
+    const newData = {
+      id: data.length ? data[data.length - 1].id + 1 : 0,
+      label: "",
+      values: "",
+    };
+    updater(data.concat(newData));
+  }
+  function updateData(e, id, key) {
+    const updatedData = data.map((item) => {
+      if (item.id === id) item[key] = e.target.value;
+      return item;
+    });
+    updater(updatedData);
   }
   function setActive(e) {
     e.preventDefault();
     toggler("skills");
+  }
+
+  function Label({ id, label }) {
+    return (
+      <label>
+        <span> Label </span>
+        <input
+          value={label}
+          onChange={(e) => updateData(e, id, "label")}
+        ></input>
+      </label>
+    );
+  }
+
+  function Values({ id, values }) {
+    return (
+      <label>
+        <span> Values </span>
+        <textarea
+          value={values}
+          onChange={(e) => updateData(e, id, "values")}
+        ></textarea>
+      </label>
+    );
+  }
+
+  function SkillGroup({ data }) {
+    return (
+      <fieldset>
+        <legend> Group </legend>
+        <Label id={data.id} label={data.label} />
+        <Values id={data.id} values={data.values} />
+      </fieldset>
+    );
   }
 
   return (
@@ -14,20 +61,12 @@ export default function Skills({ data, updater, active, toggler }) {
       </a>
       {active && (
         <>
-          <label>
-            <span> Skills </span>
-            <textarea
-              value={data.skills}
-              onChange={(e) => updateData(e, "skills")}
-            ></textarea>
-          </label>
-          <label>
-            <span> Interests </span>
-            <textarea
-              value={data.interests}
-              onChange={(e) => updateData(e, "interests")}
-            ></textarea>
-          </label>
+          <button onClick={addData}> Add group </button>
+          <ul>
+            {data.map((item) => (
+              <SkillGroup key={item.id} data={item} />
+            ))}
+          </ul>
         </>
       )}
     </section>
