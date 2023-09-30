cec772231508b4d0fa14125d028b4515d29d77bf
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-30 16:14:18 +0800

    Fix improper component calling - Education

--- src/Education.jsx
+++ src/Education.jsx
@@ -25,41 +25,6 @@ export default function Education({ data, updater, active, toggler }) {
     toggler("education");
   }
 
-  function EducationFS(data, index) {
-    return (
-      <fieldset key={data.id}>
-        <legend> Education {index + 1} </legend>
-        <InputText
-          label="School"
-          value={data.school}
-          changeHandler={(e) => editData(e, index, "school")}
-        />
-        <InputText
-          label="Course"
-          value={data.course}
-          changeHandler={(e) => editData(e, index, "course")}
-        />
-        <InputText
-          label="Degree"
-          value={data.degree}
-          changeHandler={(e) => editData(e, index, "degree")}
-        />
-        <InputText
-          label="Year Graduated / Left"
-          value={data.yearEnd}
-          changeHandler={(e) => editData(e, index, "yearEnd")}
-        />
-        <label>
-          <span> Achievements </span>
-          <textarea
-            value={data.achievements}
-            onChange={(e) => editData(e, index, "achievements")}
-          ></textarea>
-        </label>
-      </fieldset>
-    );
-  }
-
   return (
     <section className="education">
       <a href="" onClick={setActive}>
@@ -68,9 +33,51 @@ export default function Education({ data, updater, active, toggler }) {
       {active && (
         <>
           <button onClick={addData}> Add Education </button>
-          {data.map((item, index) => EducationFS(item, index))}
+          {data.map((item, index) => (
+            <EducationFS
+              key={item.id}
+              data={item}
+              {...{ index }}
+              updater={editData}
+            />
+          ))}
         </>
       )}
     </section>
+  );
+}
+
+function EducationFS({ data, index, updater }) {
+  return (
+    <fieldset>
+      <legend> Education {index + 1} </legend>
+      <InputText
+        label="School"
+        value={data.school}
+        changeHandler={(e) => updater(e, index, "school")}
+      />
+      <InputText
+        label="Course"
+        value={data.course}
+        changeHandler={(e) => updater(e, index, "course")}
+      />
+      <InputText
+        label="Degree"
+        value={data.degree}
+        changeHandler={(e) => updater(e, index, "degree")}
+      />
+      <InputText
+        label="Year Graduated / Left"
+        value={data.yearEnd}
+        changeHandler={(e) => updater(e, index, "yearEnd")}
+      />
+      <label>
+        <span> Achievements </span>
+        <textarea
+          value={data.achievements}
+          onChange={(e) => updater(e, index, "achievements")}
+        ></textarea>
+      </label>
+    </fieldset>
   );
 }
