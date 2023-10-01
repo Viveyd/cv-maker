f9e9bb7e9700b196aec534e1f7948fee3abacef8
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-02 00:37:38 +0800

    Add initial values to states

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,52 @@ function App() {
     summary: "",
     links: [],
   });
-  const [education, setEdu] = useState([]);
-  const [experience, setExp] = useState([]);
-  const [skills, setSkills] = useState([]);
-  const [achievements, setAchievements] = useState([]);
+  const [education, setEdu] = useState([
+    {
+      id: 0,
+      school: "",
+      course: "",
+      degree: "",
+      yearEnd: "",
+      achievements: "",
+    },
+  ]);
+  const [experience, setExp] = useState([
+    {
+      id: 0,
+      company: "",
+      city: "",
+      country: "",
+      position: "",
+      yearStart: "",
+      yearEnd: "",
+      achievements: "",
+    },
+  ]);
+  const [skills, setSkills] = useState([
+    {
+      id: 0,
+      label: "Skills",
+      values: "Planning, SEO, Sales, Advertising",
+    },
+    {
+      id: 1,
+      label: "Interests",
+      values: "Reddit, Korean Drama, Long distance running, Mukbang",
+    },
+  ]);
+  const [achievements, setAchievements] = useState([
+    {
+      id: 0,
+      achievement: "Lorem, ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      id: 1,
+      achievement: "Lorem, ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  ]);
   const [activeSection, setActiveSection] = useState("personal");
 
   return (
