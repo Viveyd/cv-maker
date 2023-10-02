76d5753a300ec5b06c40eca5f0400e2a02e5592a
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-02 18:03:24 +0800

    Fix mispelled icon obj

--- src/DropdownAnchor.jsx
+++ src/DropdownAnchor.jsx
@@ -1,11 +1,11 @@
 import Icon from "@mdi/react";
-import { mdilChevronDown } from "@mdi/js";
+import { mdiChevronDown } from "@mdi/js";
 
 export default function DropdownAnchor({ label, toggle }) {
   return (
     <a href="" onClick={toggle}>
       <h1> {label} </h1>
-      <Icon path={mdilChevronDown} size={1.2} />
+      <Icon path={mdiChevronDown} size={1.7} />
     </a>
   );
 }
