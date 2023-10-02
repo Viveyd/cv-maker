3f256383bb8765d659342949749f1490e197b5a2
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-02 21:00:31 +0800

    DropdownAnchor to show add btn if add prop is defined

--- src/DropdownAnchor.jsx
+++ src/DropdownAnchor.jsx
@@ -1,10 +1,20 @@
 import Icon from "@mdi/react";
-import { mdiChevronDown } from "@mdi/js";
+import { mdiChevronDown, mdiPlus } from "@mdi/js";
 
-export default function DropdownAnchor({ label, toggle }) {
+export default function DropdownAnchor({ label, toggle, add }) {
+  function addFn(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    add(e);
+  }
   return (
     <a href="" onClick={toggle}>
       <h1> {label} </h1>
+      {add != null && (
+        <button onClick={addFn}>
+          <Icon path={mdiPlus} size={1.7} />
+        </button>
+      )}
       <Icon path={mdiChevronDown} size={1.7} />
     </a>
   );
