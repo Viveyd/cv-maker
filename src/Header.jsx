435fedd85560f6967857e9770e11e82484294952
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-02 13:46:13 +0800

    Style header with font and icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,13 @@
+import Icon from "@mdi/react";
+import { mdiPrinterEye } from "@mdi/js";
+
 export default function Header({ previewMode, previewToggler }) {
   return (
     <header>
       <h1>Resume Maker</h1>
-      <a onClick={() => togglePreview(previewMode, previewToggler)}>Preview</a>
+      <a onClick={() => togglePreview(previewMode, previewToggler)}>
+        <Icon path={mdiPrinterEye} size={1.6} />
+      </a>
     </header>
   );
 }
