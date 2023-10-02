7e39ce5af0b7eca2707a1ee15e62b0e7292499d3
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-02 16:22:29 +0800

    Change preview icon when toggled

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,12 @@
 import Icon from "@mdi/react";
-import { mdiPrinterEye } from "@mdi/js";
+import { mdiPrinterEye, mdiNoteEdit } from "@mdi/js";
 
 export default function Header({ previewMode, previewToggler }) {
   return (
     <header>
       <h1>Resume Maker</h1>
       <a onClick={() => togglePreview(previewMode, previewToggler)}>
-        <Icon path={mdiPrinterEye} size={1.6} />
+        <Icon path={previewMode ? mdiNoteEdit : mdiPrinterEye} size={1.6} />
       </a>
     </header>
   );
