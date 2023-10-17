e384e66d1ccd234425dd78c48754e964d0925ad1
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-17 14:59:48 +0800

    Prevent default behavior of DeleteBtn

--- src/DeleteBtn.jsx
+++ src/DeleteBtn.jsx
@@ -3,7 +3,11 @@ import { mdiDeleteForever } from "@mdi/js";
 
 export default function DeleteBtn({ target }) {
   return (
-    <button>
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+      }}
+    >
       <Icon path={mdiDeleteForever} size={1} />
     </button>
   );
