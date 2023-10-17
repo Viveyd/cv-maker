1653d6124938057b1a2c5ceb368d42af340c2018
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-10-17 15:01:26 +0800

    Update DeleteBtn to accept click handler

--- src/DeleteBtn.jsx
+++ src/DeleteBtn.jsx
@@ -1,11 +1,12 @@
 import Icon from "@mdi/react";
 import { mdiDeleteForever } from "@mdi/js";
 
-export default function DeleteBtn({ target }) {
+export default function DeleteBtn({ onClickHandler = () => 0 }) {
   return (
     <button
       onClick={(e) => {
         e.preventDefault();
+        onClickHandler();
       }}
     >
       <Icon path={mdiDeleteForever} size={1} />
